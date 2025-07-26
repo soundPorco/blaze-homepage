8466f3717b5e4fda6444a9ec13b9710a934521cd
mizukami <porco.rosso016@gmail.com>
2025-07-26 16:48:46 +0900

    commit main.js

---
+++ main.js
@@ -0,0 +1,47 @@
+// Swiperのjs
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 3, // 例: 一度に3枚のスライドを表示
+    spaceBetween: 20, // 例: スライド間の余白を20pxに設定
+    // ページネーションが必要なら追加
+    pagination: {
+        el: ".swiper-pagination",
+        // type: "progressbar",
+    },
+    // 最後まで行くと最初の画像に戻ります
+    loop: true,
+    speed: 1500,
+    // 自動スライドです
+    autoplay: {
+        delay: 3000, // 3秒ごとにスライド
+        disableOnInteraction: false, // ユーザー操作後も自動再開
+    },
+    // ナビボタンが必要なら追加
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+// クラス紹介のカードをひっくり返すJS
+const cards = document.querySelectorAll(".class-card");
+
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
+        card.classList.toggle("flipped");
+    });
+});
+
+// クラス紹介アコーディオンタイプの実装コード
+document.querySelectorAll(".accordion-box").forEach((box) => {
+    const accordionBtn = box.querySelector(".accordion-toggle");
+    const accordionContent = box.querySelector(".accordion-content");
+
+    accordionBtn.addEventListener("click", () => {
+        accordionContent.classList.toggle("open");
+
+        accordionBtn.textContent = accordionContent.classList.contains("open")
+            ? "閉じる"
+            : "詳細を見る";
+    });
+    console.log("hello");
+});
