6b26f0d07b7dc240b0433711da81690ee55d6f3c
mizukami <porco.rosso016@gmail.com>
2025-10-04 15:51:26 +0900

    responsive fixed & add voice-section

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
 
+const slides = document.querySelectorAll(".slide");
+let current = 0;
+
+function showNextSlide() {
+    slides[current].classList.remove("active");
+    current = (current + 1) % slides.length;
+    slides[current].classList.add("active");
+}
+
+// slides[current].classList.add("active");
+// 最初のスライド表示
+// setInterval(showNextSlide, 5000);
+// 5秒ごとに切り替え
+
 // Swiperのjs
 const swiper = new Swiper(".swiper", {
     slidesPerView: 3, // 例: 一度に3枚のスライドを表示
