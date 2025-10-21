1a0378584b5805d41694c377d5bdd25e3c952a19
mizukami <porco.rosso016@gmail.com>
2025-10-21 13:31:10 +0900

    file fixed

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// ナビバーの実装
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+// クラス紹介アコーディオンタイプの実装コード
+document.querySelectorAll(".accordion-box").forEach((box) => {
+    const accordionBtn = box.querySelector(".accordion-toggle");
+    const accordionContent = box.querySelector(".accordion-content");
+
+    accordionBtn.addEventListener("click", () => {
+        accordionContent.classList.toggle("open");
+
+        accordionBtn.textContent = accordionContent.classList.contains("open")
+            ? "閉じる"
+            : "詳細を見る";
+    });
+    console.log("hello");
+});
